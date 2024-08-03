13e908186a8d412e3fc90b23a24885b1fe138337
robmllze <86869208+robmllze@users.noreply.github.com>
2024-08-03 22:07:43 +1000

    Update

--- bin/generate_dart_indexes.dart
+++ bin/generate_dart_indexes.dart
@@ -8,6 +8,8 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
 
+import 'dart:io';
+
 import 'package:df_generate_dart_indexes/df_generate_dart_indexes.dart';
 import 'package:df_log/df_log.dart';
 
@@ -15,15 +17,12 @@ import 'package:df_log/df_log.dart';
 
 void main(List<String> args) async {
   DebugLog.debugOnly = false;
-  printYellow(currentScriptDir);
+  final currentDir = Directory.current.path;
   await runGenerateIndexFilesForDartApp([
     '-t',
     '$currentScriptDir/templates/',
     '-r',
-    [
-      '"\$(pwd)"',
-      '"%cd%"',
-    ].join('&&'),
+    currentDir,
     ...args,
   ]);
 }
